dc0c1264f1ea017e28dbef2a57b9181e41fe1bc2
gylman <gylmanbm@gmail.com>
2024-12-29 00:30:38 +0900

    ref: move styles to another component

--- client/src/components/Modal.tsx
+++ client/src/components/Modal.tsx
@@ -15,61 +15,16 @@ import {
 } from "../utils/usePushNotifications";
 import { getGasPrice } from "@wagmi/core";
 import { formatEther } from "viem";
-
-const Overlay = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: 1;
-  width: 100%;
-  height: 100%;
-  background-color: rgba(0, 0, 0, 0.5);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
-const ModalContainer = styled.div`
-  width: 100%;
-  z-index: 2;
-  max-width: 500px;
-  border-radius: 10px;
-  gap: 10px;
-  padding: 30px;
-  background-color: #fff;
-  position: absolute;
-`;
-
-const Title = styled.h2`
-  margin-bottom: 20px;
-`;
-
-const InputContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-bottom: 20px;
-`;
-
-export const SelectBox = styled.select`
-  width: 100%;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 10px;
-`;
-
-const Label = styled.p``;
-
-const SubmitBtnContainer = styled.div`
-  display: flex;
-  justify-content: center;
-`;
-
-const Input = styled.input`
-  padding: 10px;
-  border-radius: 5px;
-  border: 1px solid #ccc;
-  width: 100%;
-`;
+import {
+  Overlay,
+  ModalContainer,
+  Title,
+  InputContainer,
+  SelectBox,
+  Label,
+  SubmitBtnContainer,
+  Input,
+} from "./ModalStyles";
 
 const Modal = ({ toggle }) => {
   const { chainId: accountChainId, address } = useAccount();
