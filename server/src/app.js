5337c501c671df9b1f2b019b65a6a25b74dc2bf9
gylman <gylmanbm@gmail.com>
2025-01-11 21:03:19 +0900

    WIP: errors migrating to ts

--- server/src/app.js
+++ server/src/app.js
@@ -3,11 +3,7 @@ import dotenv from "dotenv";
 import cors from "cors";
 import routes from "./routes/routes.js";
 import mongoose from "mongoose";
-import Canvas from "./models/canvasModel.js";
-import BlockSync from "./models/blockSyncModel.js";
 import watcherService from "./services/watcherService.js";
-import ERC721Service from "./services/ERC721Service.js";
-import canvasService from "./services/canvasService.js";
 
 dotenv.config({ path: "./.env" });
 
@@ -17,24 +13,15 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/v1", routes);
 
-const DB = process.env.DATABASE.replace(
-  "<PASSWORD>",
-  process.env.DATABASE_PASSWORD
-);
+const DB = process.env.DATABASE.replace("<PASSWORD>", process.env.DATABASE_PASSWORD);
 
 mongoose
   .connect(DB)
   .then(async () => {
     console.log("DB connection successful!");
-    // Clear the Canvas collection on startup
-    // await Canvas.deleteMany({});
-    // await BlockSync.deleteMany({});
-    // console.log("Canvas collection cleared");
-
-    // Start event listeners
     await watcherService.startWatchers();
   })
-  .catch((error) => {
+  .catch(error => {
     console.error("DB connection failed:", error.message);
     process.exit(1);
   });
